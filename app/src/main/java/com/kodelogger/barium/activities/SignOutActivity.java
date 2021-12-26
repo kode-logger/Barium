d84e2b730aacff9120c80fdd792b0dc45474b49c
KodeLogger <nkrishnaraj0907@gmail.com>
2021-12-26 14:33:04 +0530

    Added SignOut activity

---
+++ app/src/main/java/com/kodelogger/barium/activities/SignOutActivity.java
@@ -0,0 +1,16 @@
+package com.kodelogger.barium.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kodelogger.barium.R;
+
+public class SignOutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_out);
+    }
+}
