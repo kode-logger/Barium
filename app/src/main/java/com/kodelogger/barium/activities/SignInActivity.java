fb644cb5a26d6836c116316720befcfff45ca9e2
KodeLogger <nkrishnaraj0907@gmail.com>
2021-12-26 14:30:44 +0530

    Added SignIn activity

---
+++ app/src/main/java/com/kodelogger/barium/activities/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.kodelogger.barium.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kodelogger.barium.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
