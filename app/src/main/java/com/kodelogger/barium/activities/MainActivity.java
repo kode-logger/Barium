49b68d94a1c028f81d12d0f3242b160c42f19f68
KodeLogger <nkrishnaraj0907@gmail.com>
2021-12-27 22:34:44 +0530

    Added Firebase, Utils. Working Sign In and Sign Up

--- app/src/main/java/com/kodelogger/barium/activities/MainActivity.java
+++ app/src/main/java/com/kodelogger/barium/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.kodelogger.barium;
+package com.kodelogger.barium.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.kodelogger.barium.R;
 
 public class MainActivity extends AppCompatActivity {
 
