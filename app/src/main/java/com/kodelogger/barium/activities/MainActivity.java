1339b1c9756331dc31c31532518fd4b79af4bd9c
KodeLogger <nkrishnaraj0907@gmail.com>
2021-12-28 16:48:47 +0530

    Updated MainActivity and Constants to support SignOut

--- app/src/main/java/com/kodelogger/barium/activities/MainActivity.java
+++ app/src/main/java/com/kodelogger/barium/activities/MainActivity.java
@@ -1,16 +1,79 @@
 package com.kodelogger.barium.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Base64;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.kodelogger.barium.databinding.ActivityMainBinding;
+import com.kodelogger.barium.utils.Constants;
+import com.kodelogger.barium.utils.PreferenceManager;
 
-import com.kodelogger.barium.R;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        loadUserData();
+        getToken();
+        setListeners();
+    }
+
+    private void loadUserData() {
+        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        binding.profileImage.setImageBitmap(bitmap);
+    }
+
+    private void setListeners() {
+        binding.signOutImage.setOnClickListener(v -> signOut());
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void getToken() {
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(this::updateToken);
+    }
+
+    private void updateToken(String token) {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USERS)
+                .document(preferenceManager.getString(Constants.KEY_USER_ID));
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+                .addOnSuccessListener(unused -> showToast("Token Updated Successfully"))
+                .addOnFailureListener(e -> showToast("Unable to Update Token"));
+    }
+
+    private void signOut() {
+        showToast("Signing Out ...");
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USERS)
+                .document(preferenceManager.getString(Constants.KEY_USER_ID));
+        HashMap<String, Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates).addOnSuccessListener(unused -> {
+            preferenceManager.clear();
+            startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+            finish();
+        }).addOnFailureListener(e -> showToast("Unable to Sign Out"));
     }
 }
