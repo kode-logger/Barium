9b8cc6af2a3fd61ced5eb61e61ae58da59fd001f
KodeLogger <nkrishnaraj0907@gmail.com>
2021-12-28 20:40:35 +0530

    Minor Updates, Added new packages, drawables

---
+++ app/src/main/java/com/kodelogger/barium/activities/UserActivity.java
@@ -0,0 +1,16 @@
+package com.kodelogger.barium.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kodelogger.barium.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
