dbafcba749e1025dc2e671929b0584fa96369f70
KodeLogger <nkrishnaraj0907@gmail.com>
2021-12-29 18:20:18 +0530

    Updated Basic Chat System

--- app/src/main/java/com/kodelogger/barium/model/ChatMessage.java
+++ app/src/main/java/com/kodelogger/barium/model/ChatMessage.java
@@ -1,5 +1,8 @@
 package com.kodelogger.barium.model;
 
+import java.util.Date;
+
 public class ChatMessage {
     public String senderId, receiverId, message, dateTime;
+    public Date dateObject;
 }
